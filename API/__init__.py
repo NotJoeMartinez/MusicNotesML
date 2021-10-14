ba57d564bc533ec7a59b66a763e4c5ba79958fcc
notjoemartinez <39905973+NotJoeMartinez@users.noreply.github.com>
2021-10-14 02:00:32 -0500

    api now returns base64 encoded overlay image allong with labels

--- API/__init__.py
+++ API/__init__.py
@@ -1,18 +1,24 @@
-
-import uuid
+import os, uuid
 import datetime as dt
 from flask import Flask, request, jsonify
 from PIL import Image
 import base64
-
+from .predictions import make_predictions
+import subprocess
 
 app = Flask(__name__)
 
 @app.route("/get_notes", methods=["POST", "GET"])
 def process_image():
 
-    img_data = request.json['image']
+    secret_key_attempt = request.json["secret_key"]
+    os_secret_key = os.environ['MSHACK_KEY']
 
+    if secret_key_attempt != os_secret_key:
+        print(secret_key_attempt)
+        return "NOPE!!!"
+
+    img_data = request.json['image']
     image_path = "API/uploads/"
     filename = f"{str(uuid.uuid1())}.png"
     full_img_path = image_path + filename
@@ -21,8 +27,17 @@ def process_image():
         img_data = base64.b64decode(img_data)
         f.write(img_data)
 
+    preds_dict = make_predictions(full_img_path)
+
+    with open(preds_dict["overlayed_img_path"], "rb") as f:
+        b64_overlayed_img = base64.b64encode(f.read())
+        b64_overlayed_img = b64_overlayed_img.decode('utf-8')
+
+
     return jsonify({
-                    'image_path': full_img_path 
+                    'image_path': full_img_path,
+                    "notes_arr" : preds_dict["notes_arr"],
+                    "b64_overylayed_img": b64_overlayed_img
                 })
 
 
--- scripts.sh
+++ scripts.sh
@@ -1,9 +1,8 @@
 
 send_post(){
-	# foo=$(base64 -i test_imgs/04.PNG)
-	# curl -X POST \"$foo\" http://127.0.0.1:5000/get_string
-	# curl --data 'data=@b64.txt' -X POST http://127.0.0.1:5000/get_string
-	curl -X POST -H "Content-Type: application/json" -d  '{"image" : "'"$( base64 test_imgs/01.png)"'"}' http://127.0.0.1:5000/get_notes
+	curl -X POST -H "Content-Type: application/json" -d \
+	'{"image" : "'"$( base64 mozart/test_imgs/01.png)"'", "secret_key" : "'"$MSHACK_KEY"'"}'\
+	http://127.0.0.1:5000/get_notes
 }
 
 run_flask(){
